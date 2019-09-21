5694da186f003c9323794bbd8433f7ae1463b55c
kam7411 <natthaphat.wa@ku.th>
2019-09-21 14:21:51 +0700

    test create server

---
+++ Delicipe/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//Init App
+const app = express();
+
+//Load view Enging
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+//Home Route
+app.get('/', function(req,res){
+    res.render('index', {
+        title:'Delicipe'
+    });
+});
+
+//Start Server
+app.listen(8000,function(){
+    console.log('Server started on port 8000...')
+});

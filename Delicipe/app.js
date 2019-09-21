479ff2d6cf12991846106a9c5e7d1bcfd34f06dd
kam7411 <natthaphat.wa@ku.th>
2019-09-21 14:52:44 +0700

    เผลอแก้ ใน branch develop

--- Delicipe/app.js
+++ Delicipe/app.js
@@ -10,11 +10,14 @@ app.set('view engine','pug');
 
 //Home Route
 app.get('/', function(req,res){
-    res.render('index', {
+    res.render('main', {
         title:'Delicipe'
     });
 });
 
+app.get('/', function(req,res){
+
+})
 //Start Server
 app.listen(8000,function(){
     console.log('Server started on port 8000...')

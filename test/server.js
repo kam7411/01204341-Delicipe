0b49496df2f6e6fbf8a39601ad743ab28472315e
kam7411 <natthaphat.wa@ku.th>
2019-10-21 14:51:54 +0700

    clear git

--- test/server.js
+++
@@ -1,7 +0,0 @@
-// Crate server
-var http = require('http');
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/plain'});
- res.end('Delicipe');
-}).listen(8000, 'localhost');
-console.log('Server running at http://localhost:8000/');

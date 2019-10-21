d945c46dbc55ff1c820303a3355adb134a761e6b
kam7411 <natthaphat.wa@ku.th>
2019-10-21 14:19:58 +0700

    delete Ai not use

--- dessert/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dessert from './Dessert';
-import './index.css';
-
-ReactDOM.render(
-  <Dessert />,
-  document.getElementById('root')
-);

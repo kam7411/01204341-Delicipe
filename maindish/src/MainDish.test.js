724d887116e64d5a61a0807287b46174882480b6
kam7411 <natthaphat.wa@ku.th>
2019-10-21 14:22:05 +0700

    delete Ai file

--- maindish/src/MainDish.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainDish from './MainDish';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainDish />, div);
-});

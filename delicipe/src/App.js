84441105cd561090d00c12497871ef4b4f73cb73
kam7411 <natthaphat.wa@ku.th>
2019-10-23 15:44:44 +0700

    update create new account page

--- delicipe/src/App.js
+++ delicipe/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './Login.js';
 import Register from './Register.js';
-import Taskbar from './taskbar.js'
+import Taskbar from './taskbar.js';
+import New_Account from './New_Account.js';
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <Switch>
                 <Route path='/login' component={Login} />
                 <Route path='/register' component={Register} />
+                <Route path='/create_new_account' component={New_Account} />
             </Switch>
           </div>
         </Router>

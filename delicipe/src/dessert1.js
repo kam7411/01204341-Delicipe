73d0c57afc8cb8a8c48fbe8ada74e5f7b4accb31
NetTsuZ <net-savita@hotmail.com>
2019-10-24 11:12:07 +0700

    finish dessert1

---
+++ delicipe/src/dessert1.js
@@ -0,0 +1,202 @@
+import React, { Component } from 'react';
+import './dessert1.css';
+import island from '../picture/island.svg';
+import gluten from '../picture/gluten.svg';
+import vegan from '../picture/vegan.svg';
+import cook from '../picture/cook.svg';
+import level from '../picture/level.svg';
+import dish from '../picture/dish.svg';
+import delicipe_sign from '../picture/Delicipe.svg';
+import chefhat from '../picture/chef-hat.svg';
+import search from '../picture/search.svg';
+import accountpic from '../picture/accountpic.svg';
+import cartpic from '../picture/cartpic.svg';
+import QandA from '../picture/QandA.svg';
+import 'typeface-arvo';
+// npm install --save typeface-arvo
+
+
+class dessert1 extends Component {
+  // <div class="func">
+  //   function weightConverter(valNum) {
+  //     document.getElementById("outputKilograms").innerHTML=valNum/2.2046;
+  //   }
+  // </div>
+  render() {
+    return (
+      <div class="background">
+        <div class="topbar">
+          <img src={delicipe_sign} class="delicipe_sign" alt="delicipe_sign" />
+          <img src={search} class="search" alt="search" />
+          <img src={accountpic} class="accountpic" alt="accountpic" />
+          <img src={cartpic} class="cartpic" alt="cartpic" />
+          <div class="topnav">
+            <div class="search-container">
+              <form action="/action_page.php">
+                <button class="account" type="submit">Account</button>
+                <button class="rontine" type="submit">Rontine</button>
+                <button class="onetime" type="submit">One-time purchase</button>
+                <button class="cart" type="submit">Cart</button>
+                <input type="text" placeholder="Search.." name="search" />
+              </form>
+            </div>
+            <img src={chefhat} class="chefhat" alt="chefhat" />
+          </div>
+        </div>  {/* topbar */}
+
+        <div class="topnav"> 
+          <div class="search-container">
+            <form action="/action_page.php">
+              <button class="home" type="submit">Home</button>
+              <button class="maindish" type="submit">Maindish</button>
+              <button class="dessert" type="submit">Dessert</button>
+              <button class="howitwork" type="submit">How it work?</button>
+              <input type="text" placeholder="Search.." name="search" />
+            </form>
+          </div>
+        </div>  
+
+        <div class="bottombar"></div>
+        <div class="bottomnav">
+          <div class="search-container">
+            <form action="/action_page.php">
+              <button class="EN" type="submit">EN</button>
+              <button class="TH" type="submit">TH</button>
+            </form>
+          </div>
+        </div>
+        <img src={QandA} className="QandA" alt="QandA" />
+        {/* end of taskbar */}
+        
+        <img src={island} className="island" alt="island"/>
+        <div class="topic">FLOATING ISLANDS</div>
+        
+        <img src={gluten} className="pic_gluten" alt="gluten"/>
+        <div class="name_gluten">: GLUTEN-FREE</div>
+        <img src={vegan} className="pic_vegan" alt="vegan"/>
+        <div class="name_vegan">: VEGETARIAN</div>
+        
+        <div class="frame_price"></div>
+        <div class="price">90.00 ฿</div>
+        
+        <div class="banner">Quantity</div>
+        <div class="converter">
+          <input id="inputQuantity" type="number" placeholder="Quantity" min="1" oninput="priceConverter(this.value)" onchange="priceConverter(this.value)"/>
+        </div>
+        <button class="button" type="button">ADD TO CART</button>
+        
+        <img src={cook} className="pic_cook" alt="cook"/>
+        <div class="detail_cook">COOK : 40 MINS</div>
+        <img src={level} className="pic_level" alt="level"/>
+        <div class="detail_level">EASY</div>
+        <img src={dish} className="pic_dish" alt="dish"/>
+        <div class="detail_dish">SERVES 4</div>
+
+        <div class="zebra">
+          <div class="topic_nutrition">Nutrition</div>
+          <div class="detail_nutrition">: Per serving</div>
+          <table class="table_nutrition">
+            <tr>
+              <td>Calories(kcal)</td>
+              <td>383</td>
+            </tr>
+            <tr>
+              <td>Fat(g)</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>Carbs(g)</td>
+              <td>58</td>
+            </tr>
+            <tr>
+              <td>Protien(g)</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>Saturates(g)</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>Sugars (g)</td>
+              <td>58</td>
+            </tr>
+            <tr>
+              <td>Fibre (g)</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>Salt (g)</td>
+              <td>0.5</td>
+            </tr>
+          </table>
+
+          <div class="topic_equipment">Equipment</div>
+          <div class="detail_equipment">: 3</div>
+          <table class="table_equipment">
+            <tr>
+              <td>2</td>
+              <td>pan</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>large bowl</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>small bowl</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>stand mixer</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>frying pan</td>
+            </tr>
+          </table>
+        </div>
+        <div class="ingredient">
+          <div class="top_ingredient">What in the box !</div>
+          <table class="table_ingredient">
+            <tr>
+              <th>Ingrediants</th>
+              <th>Weight</th>
+            </tr>
+            <tr>
+              <th>whole milk</th>
+              <th>700 ml</th>
+            </tr>
+            <tr>
+              <th>Vanilla bean paste</th>
+              <th>2 tsp</th>
+            </tr>
+            <tr>
+              <th>large egg yolks</th>
+              <th>5</th>
+            </tr>
+            <tr>
+              <th>caster sugar</th>
+              <th>200 g</th>
+            </tr>
+            <tr>
+              <th>large egg white</th>
+              <th>1</th>
+            </tr>
+            </table>
+          </div> 
+          {/* edit  */}
+          <div class="bottom_ingredient">*The box’s included recipe instruction</div> 
+        // {/* <h2>Weight Converter</h2>
+        // <p>Type a value in the Pounds field to convert the value to Kilograms:</p>
+
+        // <p>
+        //   <label>Pounds</label>
+        //   <input id="inputPounds" type="number" placeholder="Pounds" oninput="weightConverter(this.value)" onchange="weightConverter(this.value)"/>
+        // </p>
+        // <p>Kilograms: <span id="outputKilograms"></span></p> */}
+      </div>
+    );
+  }
+}
+
+export default dessert1;

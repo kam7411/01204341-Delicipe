285e4e79d9accda3e244ee0e9b69cc227fdebeee
NetTsuZ <net-savita@hotmail.com>
2019-11-03 13:15:02 +0700

    add picture

---
+++ delicipe/src/components/picture.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import 'typeface-arvo'
+import 'typeface-archivo'
+import 'typeface-atma'
+
+import Select from 'react-select';
+import Slider from "react-slick";
+import Calendar from 'react-calendar'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+//all
+import spaghetti from "../picture/Spaghetti.svg";
+import dory from '../picture/SteamedDory.svg';
+import porkchop from '../picture/porkchops.svg';
+import tteokbokki from '../picture/tteokbokki.svg';
+import chickenrice from '../picture/chickenrice.svg';
+import caponata from '../picture/caponata.svg';
+import Burger from '../picture/Burger.svg';
+import sandwich from '../picture/Sandwich.svg';
+import cheesepiza from '../picture/cheesepizza.svg';
+import spicyone from '../picture/spicyone.svg';
+import spicytwo from '../picture/spicytwo.svg';
+import spicythree from '../picture/spicythree.svg';
+import component from "../picture/Component.svg";
+import delicipe_sign from '../picture/Delicipe.svg';
+import chefhat from '../picture/chef-hat.svg';
+import search from '../picture/search.svg';
+import accountpic from '../picture/accountpic.svg';
+import cartpic from '../picture/cartpic.svg';
+import homebuttonpic from '../picture/homebuttonpic.svg';
+import maindishbuttonpic from '../picture/maindishbuttonpic.svg';
+import dessertbuttonpic from '../picture/dessertbuttonpic.svg';
+import howitworkbuttonpic from '../picture/howitworkbuttonpic.svg';
+import QandA from '../picture/QandA.svg';
+import pannacotta from '../picture/pannacotta.svg';
+import tiramisu from '../picture/tiramisu.svg';
+import cheesecake from '../picture/cheesecake.svg';
+import thaibanana from '../picture/thaibanana.svg';
+import floating from '../picture/floating.svg';
+import waffles from '../picture/waffles.svg';
+import crepes from '../picture/crepes.svg';
+import icecream from '../picture/icecream.svg';
+import smoothie from '../picture/smoothie.svg';
+import wall from '../picture/wall.svg';
+import wallroutine from '../picture/wallroutine.svg';
+import cook from '../picture/cook.svg';
+import level from '../picture/level.svg';
+import profilepic from '../picture/profilepic.svg';
+// account
+import toriko from "../picture/toriko.svg";
+//cart
+import cart from "../picture/Cart.svg"
+//home
+import wallhome from '../picture/wallhome.svg'
+import ingre from '../picture/ingre.svg';
+import daily_menu_main from '../picture/daily-menu.svg';
+import daily_menu_dessert from '../picture/daily-dessert.svg';
+import checked1 from '../picture/checked1.svg';
+import checked2 from '../picture/checked2.svg';
+import checked3 from '../picture/checked3.svg';
+import ingredient from '../picture/ingredient.svg';
+import recipe from '../picture/recipe.svg';
+import guide from '../picture/guide.svg';
+//peyment
+import paypal from '../picture/paypal.svg';
+import visa from '../picture/visa.svg';
+import tag from '../picture/tag.svg';
+//paysuccess
+import success_sign from '../../picture/success.gif'
+//QA
+import khem from '../picture/khem.svg';
+import ai from '../picture/ai.png';
+import oom from '../picture/oom.png';
+import nune from '../picture/nune.png';
+import net from '../picture/net.png';
+//maindish1
+import rice from '../picture/rice.svg';
+import muslim from '../picture/muslim.svg';
+//dessert1
+import island from '../picture/island.svg';
+import gluten from '../picture/gluten.svg';
+import vegan from '../picture/vegan.svg';
+import dish from '../picture/dish.svg';
+
